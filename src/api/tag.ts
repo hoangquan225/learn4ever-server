2cc57ad69b3e1f78edc1fa61d4e54e1276a206fd
Hoàng Quân <hoangquan225.qh@gmail.com>
2022-12-30 01:17:51 +0700

    add api tag

---
+++ src/api/tag.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import TagService from '../services/tag';
+import asyncHandler from '../utils/async_handle';
+import Endpoint from '../submodule/common/endpoint';
+import TTCSconfig from '../submodule/common/config'
+
+const tagRouter = express.Router();
+const tagService = new TagService();
+
+tagRouter.post(Endpoint.GET_TAGS_BY_STATUS, asyncHandler(async (req, res) => {
+    const data = await tagService.getTagsByStatus({status : Number(req.query.status)})
+    return res.json({
+        data,
+        status : TTCSconfig.STATUS_SUCCESS
+    })
+}))
+
+tagRouter.post(Endpoint.UPDATE_TAG, asyncHandler(async (req, res) => {
+    const data = await tagService.updateTag(req.body)
+    return res.json(data)
+}))
+
+export { tagRouter };
