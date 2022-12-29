eb2937dcacb0bf1b41ad65e56ca875400f4b3eb3
Nguyen Duy Hung <duyhungfa102@gmail.com>
2022-12-30 00:55:33 +0700

    update

--- index.ts
+++ index.ts
@@ -42,7 +42,7 @@ class App {
     }
     run() {
         connectDatabase(() => {
-            this.server.listen(this.port, () => {
+            this.server.listen(process.env.PORT || this.port, () => {
                 logger.info(`Server is running on port ${this.port}`);
             });
             // initSocket(this.server);
