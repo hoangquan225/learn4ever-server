b0f4f2627542441a1a1c053fb2daa1a384bc4435
Hoàng Quân <hoangquan225.qh@gmail.com>
2022-12-30 14:49:20 +0700

    update router.use()

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,22 @@ import { Router } from 'express';
 import { authRouter } from '../api/web/auth';
 import { userRouter } from '../api/user';
 import { categoryRouter } from '../api/category';
+import { courseRouter } from '../api/course';
+import { lessonRouter } from '../api/lesson';
+import { questionRouter } from '../api/question';
+import { tagRouter } from '../api/tag';
+import { topicRouter } from '../api/topic';
 
 const router = Router();
 
 router.use(authRouter);
 router.use(userRouter);
 router.use(categoryRouter);
+
+router.use(courseRouter);
+router.use(lessonRouter);
+router.use(questionRouter);
+router.use(tagRouter);
+router.use(topicRouter);
 
 export { router as webRouters };
