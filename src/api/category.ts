1736a853fa744386570599bbfe8dbd8388925314
Nguyen Duy Hung <duyhungfa102@gmail.com>
2023-01-01 12:04:08 +0700

    update category api

--- src/api/category.ts
+++ src/api/category.ts
@@ -8,21 +8,26 @@ const categoryRouter = express.Router();
 const categoryService = new CategoryService();
 
 categoryRouter.post(Endpoint.GET_CATEGORYS_BY_STATUS, asyncHandler(async (req, res) => {
-    const body  = req.body;
+    const body = req.body;
     const data = await categoryService.getCategorysByStatus(
-        body.status 
-        ? {status : Number(body.status)} 
-        : {status : Number(req.query.status)}
+        body.status
+            ? { status: Number(body.status) }
+            : { status: Number(req.query.status) }
     )
-    
+
     return res.json({
         data,
-        status : TTCSconfig.STATUS_SUCCESS
+        status: TTCSconfig.STATUS_SUCCESS
     })
 }))
 
 categoryRouter.post(Endpoint.UPDATE_CATEGORY, asyncHandler(async (req, res) => {
     const data = await categoryService.updateCategory(req.body)
+    return res.json(data)
+}))
+
+categoryRouter.post(Endpoint.ORDER_CATEGORY, asyncHandler(async (req, res) => {
+    const data = await categoryService.orderCategory(req.body)
     return res.json(data)
 }))
 
