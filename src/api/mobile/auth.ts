28a39da97bae31aed2394ff02b42af9f4b1e28f5
Nguyen Duy Hung <duyhungfa102@gmail.com>
2023-09-07 23:08:06 +0700

    add middle ware and api user mobile

--- src/api/mobile/auth.ts
+++ src/api/mobile/auth.ts
@@ -7,6 +7,8 @@ import { jwtDecodeToken, jwtEncode } from "../../utils/jwtToken";
 import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import { BadRequestError } from "../../common/errors";
 import { UserInfo } from "../../submodule/models/user";
+import { jwtMiddleware } from "./middleware";
+import _ from "lodash";
 
 const router = Router();
 
@@ -38,21 +40,8 @@ router.post("/login", async_handle(async (req, res) => {
     })
 }))
 
-router.post("/session", async_handle(async (req, res) => {
-    try {
-        const { token } = req.body;
-        if (!token) return res.status(401).json({})
-        const decode = jwtDecodeToken(token);
-        if (!decode || typeof decode === "string") return res.status(401).json({});
-        return res.status(200).json({});
-    } catch (error) {
-        if (error instanceof JsonWebTokenError) {
-            if (error instanceof TokenExpiredError) {
-                return res.status(401).json({ data: -1 }); // token het han
-            }
-        }
-        throw new BadRequestError();
-    }
+router.post("/session", jwtMiddleware, async_handle(async (req, res) => {
+    return res.status(200).json({})
 }))
 
 router.post("/register", async_handle(async (req, res) => {
@@ -82,6 +71,13 @@ router.post("/register", async_handle(async (req, res) => {
         status: TTCSconfig.STATUS_SUCCESS,
         token
     })
+}))
+
+router.post("/user", jwtMiddleware, async_handle(async (req, res) => {
+    const { _id } = req.body;
+    const user = await UserModel.findOne({ _id });
+
+    return res.json(user)
 }))
 
 export { router as authRouter }
